2e11feeeffea2ed88003505875dd0cdf28624c16
Billy DeLashmutt <delashmuttwa@gmail.com>
2021-12-24 18:29:45 -0500

    Complete. Code page and repo add TBD.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,36 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hey World!");
+// Get string from the page
+// Controller Function
+function getValue() {
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+// Reverse string
+// Logic Function
+function reverseString(userString) {
+
+    let revString = [];
+
+    // Reverse string via for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+
+    return revString;
+}
+
+// Display reversed string to user
+// View Function
+function displayString(revString) {
+
+    // Write message to page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    // Show alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
